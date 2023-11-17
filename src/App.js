b50bb4f1663feb8a2d4cdc8c31d909848afa56f1
Angel <ata@loriot.io>
2023-11-17 17:31:20 +0100

    Games list * API call effect implementation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const res = await fetch('http://localhost:3001/videogames');
+      const data = await res.json();
+      setGames(data.results);
+      console.log(games)
+    }
+
+    getData();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Games</h1>
+      <ul>
+        {games.map(game => <li key={game.id}>{game.name}</li>)}
+      </ul>
     </div>
   );
 }
