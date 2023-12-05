2c5c00ebf3c2c3d13efda06d245eada9cad0f284
Angel <atalavera86@gmail.com>
2023-12-05 11:01:46 +0100

    Game Details Form * WIP for submittion prevent navigation

--- src/features/edit-game/game-details-modal.js
+++ src/features/edit-game/game-details-modal.js
@@ -6,9 +6,16 @@ import { UserGamesContext } from '../../pages/user-games/user-games-context';
 export default function GameDetailsModal() {
   const {modalOpen} = useContext(UserGamesContext);
 
+  function submitHandler(event) {
+    event.preventDefault();
+  }
+
   return (
     <Modal open={modalOpen}>
       <h1>Game Details</h1>
+      <form onSubmit={submitHandler}>
+
+      </form>
     </Modal>
   );
 }
