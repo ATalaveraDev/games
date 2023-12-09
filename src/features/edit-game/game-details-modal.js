9e601e917cdc5b3065098c9a99fb4f4f30984eff
Angel <atalavera86@gmail.com>
2023-12-09 06:11:43 +0100

    Game details form * Added fields

--- src/features/edit-game/game-details-modal.js
+++ src/features/edit-game/game-details-modal.js
@@ -1,10 +1,11 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 import Modal from '../ui/modal';
 import { UserGamesContext } from '../../pages/user-games/user-games-context';
 
 export default function GameDetailsModal() {
   const {modalOpen} = useContext(UserGamesContext);
+  const [game, setGame] = useState({});
 
   function submitHandler(event) {
     event.preventDefault();
@@ -14,7 +15,16 @@ export default function GameDetailsModal() {
     <Modal open={modalOpen}>
       <h1>Game Details</h1>
       <form onSubmit={submitHandler}>
-
+        <label htmlFor="status">Status</label>
+        <select id="status">
+          <option value="">Select an option</option>
+          <option value="NOT_STARTED">Not started</option>
+          <option value="IN_PROGRESS">In Progress</option>
+          <option value="FINISHED">Finished</option>
+        </select>
+        <label htmlFor="platform">Platform</label>
+        <input type="text" />
+        <button>Accept</button>
       </form>
     </Modal>
   );
